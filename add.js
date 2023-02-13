fe12266fc37f086ffa34f903c2b161621a603c90
vedhamni <nive2682001@gmail.com>
2023-02-13 19:29:29 +0530

    oddoreven.js is added

--- add.js
+++
@@ -1,5 +0,0 @@
-let x = 4;
-let y = 7;
-
-// addition
-console.log('x + y = ', x + y);  // 11
---
+++ oddoreven.js
@@ -0,0 +1,12 @@
+function isEvenorOdd(x) {
+
+  if(x % 2 == 0){
+    console.log("x is a Even number")
+  }
+  else{
+    console.log("x is a Odd number")
+  }
+}
+
+isEvenorOdd(18) //"18 is a Even number"
+isEvenorOdd(7) //"7 is a Odd number"

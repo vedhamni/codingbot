b925998c6483aa92dd777c6d90c6870dfc7ac331
vedhamni <nive2682001@gmail.com>
2023-02-13 13:41:55 +0530

    operators.js is added

---
+++ implicit.js
@@ -0,0 +1,10 @@
+let result;
+
+result = '7' - true;
+console.log(result); // 6
+
+result = 7 + true;
+console.log(result); // 8
+
+result = 7 + false;
+console.log(result); // 7
---
+++ operators.js
@@ -0,0 +1,20 @@
+let x = 7;
+let y = 4;
+
+// addition
+console.log('x + y = ', x + y);  // 11
+
+//subtraction
+console.log('x - y = ', x - y); //3
+
+//multiplication
+console.log('x * y = ', x * y); //28
+
+//division
+console.log('x / y = ' , x / y); //1.75
+
+//remainder
+console.log('x % y = ' , x % y); //3
+
+//exponentiation
+console.log('x ** y = ' , x ** y); //2401

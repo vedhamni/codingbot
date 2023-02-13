fd9aef809d95336cfe94cd46e0ea4d75ac498aee
vedhamni <nive2682001@gmail.com>
2023-02-13 20:34:17 +0530

    largestoftwo.js is added

---
+++ largestofthree.js
@@ -0,0 +1,16 @@
+function findLargest(x1, x2, x3) {
+
+    if(x1 > x2 && x1 > x3){
+      console.log("x1 is the largest number")
+    }
+    else if (x2 > x3){
+      console.log("x2 is the largest number")
+    }
+    else{
+      console.log("x3 is the largest number")
+    }
+  }
+  
+  findLargest(26,14,8) //26 is the largest number"
+  findLargest(20,40,16) //40 is the largest number"
+  findLargest(8,6,49) //48 is the largest number"
---
+++ largestoftwo.js
@@ -0,0 +1,13 @@
+function findLargest(x1, x2) {
+
+    if(x1 > x2){
+      console.log("x1 is the largest number")
+    }
+    else if (x2> x1){
+      console.log("x2 the largest number")
+    }
+  }
+  
+  findLargest(14,7) //14 the largest number"
+  findLargest(12,18) //18 is the largest number"
+  

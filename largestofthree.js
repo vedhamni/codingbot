bf73be742687fe4d51c81bd0cfedb9b4361b5e75
Pandiya rajan A <51842349+Pandicool@users.noreply.github.com>
2023-02-14 19:48:58 +0530

    added dynamic fn

--- largestofthree.js
+++ largestofthree.js
@@ -10,7 +10,24 @@ function findLargest(x1, x2, x3) {
       console.log("x3 is the largest number")
     }
   }
+
+function dynamicFindLargest(x1, x2, x3) {
+
+    if(x1 > x2 && x1 > x3){
+      console.log(`${x1} is the largest number`);
+    }
+    else if (x2 > x3){
+      console.log(`${x2} is the second largest number`);
+    }
+    else{
+      console.log(`${x3} is the smallest number`);
+    }
+  }
   
   findLargest(26,14,8) //26 is the largest number"
   findLargest(20,40,16) //40 is the largest number"
   findLargest(8,6,49) //48 is the largest number"
+
+dynamicFindLargest(26,14,8) //26 is the largest number"
+dynamicFindLargest(20,40,16) //40 is the largest number"
+dynamicFindLargest(8,6,49) //48 is the largest number"

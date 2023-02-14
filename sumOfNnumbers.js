e11e60d05e54c54f960fd27b69904efbe22b7503
vedhamni <nive2682001@gmail.com>
2023-02-14 16:37:58 +0530

    sumOfNnumbers.js is added

---
+++ sumOfNnumbers.js
@@ -0,0 +1,9 @@
+//sum of n numbers
+let sum = 0;
+const n = 100;
+//in each step i is increased by 1
+for(let i = 1; i <= n; i++) {
+    sum += i; //sum = sum + i
+}
+    console.log('sum:', sum);
+

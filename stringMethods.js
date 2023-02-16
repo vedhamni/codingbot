ac9cc24ab19d0e61664e040d5a811c29aba866fc
vedhamni <nive2682001@gmail.com>
2023-02-16 17:16:26 +0530

    stringMethods.js is added

---
+++ stringMethods.js
@@ -0,0 +1,27 @@
+const a1 = 'bow';
+const a2 = 'boww';
+const a3 = '   Javascript   ';
+
+//concat 2 str
+const y1 = a1.concat(' ', a2);
+console.log(y1);
+
+//conversion of test into uppercase
+const y2 = a1.toUpperCase();
+console.log(y2);
+
+//removing whitespace from str
+const y3 = a3.trim();
+console.log(y3);
+
+//conversion of str in to arr[]
+const y4 = a1.split();
+console.log(y4);
+
+//str slicing
+const y5 = a1.slice(1,3);
+console.log(y5);
+
+
+
+

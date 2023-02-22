0ce31f187734ffa79b4272ba4026e0f72085e715
vedhamni <nive2682001@gmail.com>
2023-02-22 15:59:45 +0530

    adding the flyodtriangle

---
+++ objects/constructor.js
@@ -0,0 +1,18 @@
+var my_car = function(name, mileage, max_speed){
+
+    this.name = Exent;
+    this.mileage = 250;
+    this.max_speed = 10;
+
+};
+
+
+
+function processData(input) {
+    var my_car = new my_car(input[0], input[1], input[2]) 
+    console.log(my_car.name);
+    console.log(my_car.mileage);
+    console.log(my_car.max_speed);
+} 
+
+
